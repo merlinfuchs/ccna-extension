0a48b5ffb0c105f11542dafaf4caa17ce877945e
leakitisrossi <112285026+leakitisrossi@users.noreply.github.com>
2023-05-18 17:34:01 +0200

    improvements

--- extension/content.js
+++ extension/content.js
@@ -2,12 +2,18 @@ let answerUrl;
 let chapterData;
 
 function fetchChapterData() {
-    // Chapter answers are fetched by the background script because CORS sucks
-    chrome.runtime.sendMessage({type: "load", url: answerUrl}, data => {
+    return new Promise((resolve, reject) => {
+      chrome.runtime.sendMessage({ type: "load", url: answerUrl }, (data) => {
         console.table(data);
-        chapterData = data;
+        if (data) {
+          resolve(data);
+        } else {
+          reject(new Error("Failed to fetch chapter data."));
+        }
+      });
     });
-}
+  }
+  
 
 function matchText(textA, textB) {
     const replaceRegex = /[^\w]/gi;
@@ -16,57 +22,55 @@ function matchText(textA, textB) {
     return (textA === textB);
 }
 
-
-function findAnswers(questionText, answers) {
-    if (chapterData === null) {
-        alert("No chapter data loaded. Maybe the fetch failed?!");
-        return [];
-    }
-
+function findAnswers(questionText, answers, chapterData) {
     const correctAnswers = [];
     for (let entry of chapterData) {
-        if (matchText(questionText.trim(), entry.question)) {
-            for (let availableAnswer of answers) {
-                for (let possibleAnswer of entry.answers) {
-                    if (matchText(availableAnswer.textContent.trim(), possibleAnswer)) {
-                        correctAnswers.push(availableAnswer);
-                    }
-                }
+      if (matchText(questionText.trim(), entry.question)) {
+        for (let availableAnswer of answers) {
+          for (let possibleAnswer of entry.answers) {
+            if (matchText(availableAnswer.textContent.trim(), possibleAnswer)) {
+              correctAnswers.push(availableAnswer);
             }
+          }
         }
+      }
     }
-
     return correctAnswers;
-}
-
-
-function processQuestion(question) {
+  }
+  
+  async function processQuestion(question) {
     const questionTextDom = question.querySelector(".questionText .mattext");
     if (!questionTextDom) return;
     const questionText = questionTextDom.textContent.trim();
-
+  
     const answersDom = question.querySelector("ul.coreContent");
     if (!answersDom) return;
     const answers = answersDom.children;
-
+  
     for (let answer of answers) {
-        const input = answer.querySelector("input");
-        if (!input) continue;
-        input.checked = false;
+      const input = answer.querySelector("input");
+      if (!input) continue;
+      input.checked = false;
     }
-
-    const correctAnswers = findAnswers(questionText, answers);
-    if (correctAnswers.length === 0) {
+  
+    try {
+      const chapterData = await fetchChapterData();
+      const correctAnswers = findAnswers(questionText, answers, chapterData);
+      if (correctAnswers.length === 0) {
         console.log("No answers found for this question. ;(");
         return;
-    }
-
-    for (const answer of correctAnswers) {
+      }
+  
+      for (const answer of correctAnswers) {
         const input = answer.querySelector("input");
         if (!input) continue;
         input.checked = true;
+      }
+    } catch (error) {
+      console.error("Error fetching chapter data:", error);
     }
-}
+  }
+  
 
 
 function clickNext() {
@@ -91,4 +95,11 @@ window.addEventListener("keydown", event => {
             fetchChapterData();
         })
     }
+    else if (event.key === "t") {
+      chrome.storage.local.get(["lastUrl"], result => {
+          answerUrl = "put-link-here";
+          chrome.storage.local.set({lastUrl: answerUrl});
+          fetchChapterData();
+      })
+  }
 });
